faa9044343b83a8d7116f9a0ce5164eb82b3677f
Isoken June <isokenjune@gmail.com>
2024-07-20 05:28:23 +0100

    react immutable

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { StyleSheet, css } from "aphrodite";
 
 function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
   return (
     <React.Fragment>
       <div className={css(styles["App-body"])}>
         <p>Login to access the full dashboard</p>
-        <form>
+        <form onSubmit={handleLoginSubmit}>
           <label htmlFor="email">Email:</label>
           <input className={css(styles.input)} type="email" name="email"></input>
           <label htmlFor="password">Password:</label>
           <input className={css(styles.input)} type="password" name="password"></input>
-          <button>OK</button>
+          <input type="submit" value="ok" disabled={!enableSubmit}/>
         </form>
       </div>
     </React.Fragment>
